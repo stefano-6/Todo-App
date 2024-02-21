2a1c55fe83d893c4da212adeabdc5d37ea0d4e77
stefano-6 <93028855+stefano-6@users.noreply.github.com>
2024-02-22 09:11:52 +1100

    Add files via upload

---
+++ api/Rockfast.ServiceInterfaces/ITodoService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rockfast.ServiceInterfaces
+{
+    public interface ITodoService<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<int> Create(T entity);
+        Task<T> GetSingle(Expression<Func<T, bool>> predicate);
+        // IEnumerable<T> Query(Expression<Func<T, bool>> predicate);
+        Task<int> Update(T entity);
+        Task Remove(T entity);
+    }
+}
---
+++ api/Rockfast.ViewModels/TodoVM.cs
@@ -0,0 +1,11 @@
+﻿using Rockfast.ApiDatabase;
+using Rockfast.ApiDatabase.DomainModels;
+
+namespace Rockfast.ViewModels
+{
+    public class TodoVM
+    {
+        public Todo Task { get; set; }
+        public User User { get; set; }
+    }
+}
